59f819e32104239c1ba04ceea9a0d9b50f24dbda
8717860914 <shvyas@bestpeers.com>
2022-12-29 11:01:36 +0530

    array final

--- array1.rb
+++ array1.rb
@@ -1,2 +1,38 @@
-arr = ["shreesh",23.0,8745637812]
-puts arr
+
+shreyash = 0
+dhruv = 0
+soniya = 0
+vikas = 0
+nums = Array[
+  {name: 'Shreyash'},
+  {'name': 'Dhruv'},
+  {'name': 'Shreyash'},
+  {name: 'Vikas'},
+  {name: 'Soniya'},
+  {name: 'Vikas'}
+
+]
+len = nums.length()
+for i in 0..len do
+
+if
+shreyash= nums.count { |nums| nums[:name] == 'Shreyash' }
+elsif
+dhruv= nums.count { |nums| nums{:name == 'Dhruv' } }
+elsif
+soniya= nums.count { |nums| nums[:name] == 'Soniya' }
+elsif
+vikas= nums.count { |nums| nums[:name] == 'Vikas' }
+
+
+
+end
+end
+
+
+puts"Shreyash Printed #{shreyash} Times"
+puts"Dhruv Printed #{dhruv} Times"
+puts"Soniya Printed #{soniya} Times"
+puts"Soniya Printed #{vikas} Times"
+
+
